4f63006b45c78422586d4ca3c83160c6e2554ae9
Gregory John Casamento <greg.casamento@gmail.com>
2019-03-22 11:48:27 -0400

    Add a little bit extra

--- hello/src/hello.c
+++ hello/src/hello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+#define MAX 15
+
 int main()
 {
-  printf("Hello, world!");
+  char buf[MAX+1];
+  printf("Hello, world!\n");
+  printf("What's your name? ");
+  fgets(buf, MAX, stdin);
+  printf("Hi, %s",buf);
   return 0;
 }
